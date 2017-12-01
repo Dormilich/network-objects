ef08fed7b0f52e35c9dcfce215b9082c2a2ac05c
Bertold von Dormilich <dormilich@netscape.net>
2017-12-01 16:46:10 +0100

    created network implementation and tests.

---
+++ src/Network.php
@@ -0,0 +1,249 @@
+<?php
+
+namespace Dormilich\Http;
+
+class Network implements NetworkInterface, RangeInterface
+{
+    /**
+     * @var IpInterface The network address of the network.
+     */
+    private $network;
+
+    /**
+     * @var IpInterface The netmask of the network.
+     */
+    private $netmask;
+
+    /**
+     * @see `Network::parse()`
+     * @param mixed $input 
+     * @return self
+     */
+    public function __construct( $input )
+    {
+        $this->parse( $input );
+    }
+
+    /**
+     * Set the network and netmask properties.
+     * 
+     * @param IpInterface $network Network address.
+     * @param IpInterface $netmask Netmask address.
+     * @return void
+     */
+    private function setNetwork( IpInterface $network, IpInterface $netmask )
+    {
+        $this->network = $network;
+        $this->netmask = $netmask;
+    }
+
+    /**
+     * Convert a CIDR into a network object. If an IP is given, imply a 
+     * one-address network (/32 or /128).
+     * 
+     * @param NetworkInterface|string $input Network object, CIDR, or IP address.
+     * @return void
+     * @throws RuntimeException Input (or a part thereof) failed to parse as IP.
+     */
+    private function parse( $input )
+    {
+        if ( $input instanceof NetworkInterface ) {
+            return $this->setNetwork( $input->getNetwork(), $input->getNetmask() );
+        }
+
+        if ( is_object( $input ) and method_exists( $input, '__toString' ) ) {
+            $input = (string) $input;
+        }
+
+        if ( is_string( $input ) and preg_match( '#^[0-9a-f:.]+/\d+$#i', $input ) ) {
+            return $this->parseCIDR( $input );
+        }
+
+        $ip = new IP( $input );
+        $mask = $this->netmaskFromPrefix( $ip, $this->getMaxPrefixLength( $ip ) );
+
+        $this->setNetwork( $ip, $mask );
+    }
+
+    /**
+     * Parse a CIDR string and determine it's network address and netmask.
+     * 
+     * @param string $cidr CIDR string.
+     * @return void
+     * @throws RuntimeException Input (or a part thereof) failed to parse as IP.
+     */
+    private function parseCIDR( $cidr )
+    {
+        list( $ip, $prefix ) = explode( '/', $cidr, 2 );
+        $ip = new IP( $ip );
+
+        $netmask = $this->netmaskFromPrefix( $ip, $prefix );
+        $network_addr = $ip->inAddr() & $netmask->inAddr();
+        $network = new IP( inet_ntop( $network_addr ) );
+
+        $this->setNetwork( $network, $netmask );
+    }
+
+    /**
+     * Get the maximum prefix length for the current type of IP (IPv4/IPv6).
+     * 
+     * @param IpInterface $ip An IP address.
+     * @return integer
+     */
+    protected function getMaxPrefixLength( IpInterface $ip )
+    {
+        return 8 * strlen( $ip->inAddr() );
+    }
+
+    /**
+     * Determine the netmask from the prefix length and network type (IPv4/IPv6).
+     * 
+     * @param IpInterface $ip An IP object from the current network.
+     * @param integer $prefix The prefix length.
+     * @return IpInterface
+     * @throws RuntimeException Invalid prefix length for the IP version.
+     */
+    protected function netmaskFromPrefix( IpInterface $ip, $prefix )
+    {
+        $max = $this->getMaxPrefixLength( $ip );
+
+        if ( $prefix <= $max ) {
+            $bin = str_pad( str_repeat( '1', $prefix ), $max, '0', STR_PAD_RIGHT );
+            return new IP( $bin );
+        }
+
+        $msg = "Prefix length $prefix exceeds the maximum value of $max.";
+        throw new \RuntimeException( $msg );
+    }
+
+    /**
+     * Returns a textual representation of the network.
+     * 
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getCIDR();
+    }
+
+    /**
+     * Returns the number of IPs in the network.
+     * 
+     * @return string
+     */
+    public function count()
+    {
+        $shift = $this->getMaxPrefixLength( $this->network ) - $this->getPrefixLength();
+
+        if ( $this->network->getVersion() === 4 ) {
+            return (string) ( 1 << $shift );
+        }
+
+        return bcpow( 2, $shift );
+    }
+
+    /**
+     * Returns the network address of the network.
+     * 
+     * @return IpInterface
+     */
+    public function getNetwork()
+    {
+        return clone $this->network;
+    }
+
+    /**
+     * Returns the broadcast address of the network.
+     * 
+     * @return IpInterface
+     */
+    public function getBroadcast()
+    {
+        return $this->getLastIP();
+    }
+
+    /**
+     * Returns the CIDR of the network.
+     * 
+     * @return string
+     */
+    public function getCIDR()
+    {
+        return sprintf( '%s/%s', $this->network, $this->getPrefixLength() );
+    }
+
+    /**
+     * Returns the prefix length of the network.
+     * 
+     * @return integer
+     */
+    public function getPrefixLength()
+    {
+        return strlen( rtrim( $this->netmask->toBin(), '0' ) );
+    }
+
+    /**
+     * Returns the netmask of the network.
+     * 
+     * @return IpInterface
+     */
+    public function getNetmask()
+    {
+        return clone $this->netmask;
+    }
+
+    /**
+     * Returns a list of usable (host) IPs.
+     * 
+     * @return RangeInterface
+     */
+    public function getHosts()
+    {
+        $first = $this->getFirstIP();
+        $last  = $this->getLastIP();
+
+        if ( $this->network->getVersion() === 4 ) {
+            if ( $this->count() > 2 ) {
+                $first = substr( $first->toBin(), 0, -1 ) . '1'; // +1
+                $last  = substr( $last->toBin(),  0, -1 ) . '0'; // -1
+            }
+            else {
+                $first = $last; # /31 & /32
+            }
+        }
+
+        return new Range( $first, $last );
+    }
+
+    /**
+     * Get the start IP of the range.
+     * 
+     * @return IpInterface
+     */
+    public function getFirstIP()
+    {
+        return $this->getNetwork();
+    }
+
+    /**
+     * Get the end IP of the range.
+     * 
+     * @return IpInterface
+     */
+    public function getLastIP()
+    {
+        $last_addr = $this->network->inAddr() | ~$this->netmask->inAddr();
+
+        return new IP( inet_ntop( $last_addr ) );
+    }
+
+    /**
+     * Get a list of networks that cover the IP range.
+     * 
+     * @return NetworkInterface[]
+     */
+    public function getNetworks()
+    {
+        return [ clone $this ];
+    }
+}
---
+++ tests/NetworkTest.php
@@ -0,0 +1,118 @@
+<?php
+
+use Dormilich\Http\IP;
+use Dormilich\Http\Network;
+use Dormilich\Http\Range;
+use PHPUnit\Framework\TestCase;
+
+class NetworkTest extends TestCase
+{
+    public function testCreateIPv4NetworkFromCIDR()
+    {
+        $net = new Network( '192.168.49.3/29' );
+
+        $this->assertSame( '8', $net->count(), 'count' );
+        $this->assertSame( 29, $net->getPrefixLength(), 'prefix' );
+        $this->assertSame( '192.168.49.0/29', $net->getCIDR(), 'cidr' );
+        $this->assertSame( '192.168.49.0', (string) $net->getNetwork(), 'network' );
+        $this->assertSame( '192.168.49.7', (string) $net->getBroadcast(), 'broadcast' );
+        $this->assertSame( '255.255.255.248', (string) $net->getNetmask(), 'netmask' );
+    }
+
+    public function testCreateIPv6NetworkFromCIDR()
+    {
+        $net = new Network( '2001:0db8:85a3:08d3:1319:8a2e:0370:7347/64' );
+
+        $this->assertSame( '18446744073709551616', $net->count(), 'count' );
+        $this->assertSame( 64, $net->getPrefixLength(), 'prefix' );
+        $this->assertSame( '2001:db8:85a3:8d3::/64', $net->getCIDR(), 'cidr' );
+        $this->assertSame( '2001:db8:85a3:8d3::', (string) $net->getNetwork(), 'network' );
+        $this->assertSame( '2001:db8:85a3:8d3:ffff:ffff:ffff:ffff', (string) $net->getBroadcast(), 'broadcast' );
+        $this->assertSame( 'ffff:ffff:ffff:ffff::', (string) $net->getNetmask(), 'netmask' );
+    }
+
+    public function testCreateNetworkFromIPv4()
+    {
+        $net = new Network( '192.168.2.1' );
+
+        $this->assertSame( '1', $net->count(), 'count' );
+        $this->assertSame( 32, $net->getPrefixLength(), 'prefix' );
+        $this->assertSame( '192.168.2.1/32', $net->getCIDR(), 'cidr' );
+        $this->assertSame( '192.168.2.1', (string) $net->getNetwork(), 'network' );
+        $this->assertSame( '192.168.2.1', (string) $net->getBroadcast(), 'broadcast' );
+        $this->assertSame( '255.255.255.255', (string) $net->getNetmask(), 'netmask' );
+    }
+
+    public function testCreateNetworkFromIPv6()
+    {
+        $net = new Network( '2001:0db8:85a3:08d3:1319:8a2e:0370:7347' );
+
+        $this->assertSame( '1', $net->count(), 'count' );
+        $this->assertSame( 128, $net->getPrefixLength(), 'prefix' );
+        $this->assertSame( '2001:db8:85a3:8d3:1319:8a2e:370:7347/128', $net->getCIDR(), 'cidr' );
+        $this->assertSame( '2001:db8:85a3:8d3:1319:8a2e:370:7347', (string) $net->getNetwork(), 'network' );
+        $this->assertSame( '2001:db8:85a3:8d3:1319:8a2e:370:7347', (string) $net->getBroadcast(), 'broadcast' );
+        $this->assertSame( 'ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff', (string) $net->getNetmask(), 'netmask' );
+    }
+
+    public function testCreatePeerNetwork()
+    {
+        $net = new Network( '192.168.2.1/31' );
+
+        $this->assertSame( '2', $net->count(), 'count' );
+        $this->assertSame( 31, $net->getPrefixLength(), 'prefix' );
+        $this->assertSame( '192.168.2.0/31', $net->getCIDR(), 'cidr' );
+        $this->assertSame( '192.168.2.0', (string) $net->getNetwork(), 'network' );
+        $this->assertSame( '192.168.2.1', (string) $net->getBroadcast(), 'broadcast' );
+        $this->assertSame( '255.255.255.254', (string) $net->getNetmask(), 'netmask' );
+    }
+
+    public function testCreateNetworkFromNetworkObject()
+    {
+        $src = new Network( '192.168.49.3/29' );
+        $net = new Network( $src );
+
+        $this->assertSame( '192.168.49.0/29', $net->getCIDR() );
+    }
+
+    public function testCreateNetworkFromStringifiableObject()
+    {
+        $obj = $this->createMock( 'Exception' );
+        $obj->method( '__toString' )->willReturn( '192.168.49.3/29' );
+
+        $net = new Network( $obj );
+
+        $this->assertSame( '192.168.49.0/29', $net->getCIDR() );
+    }
+
+    /**
+     * @expectedException RuntimeException
+     * @expectedExceptionMessage Input '192.168.31.240 - 192.168.35.193' of type [string] could not be converted into an IP object.
+     */
+    public function testCreateNetworkFromRangeFails()
+    {
+        new Network( '192.168.31.240 - 192.168.35.193' );
+    }
+
+    public function getIPv4Hosts()
+    {
+        $net = new Network( '192.168.49.3/29' );
+        $hosts = $net->getHosts();
+
+        $this->assertInstanceOf( Range::class, $hosts );
+        $this->assertSame( '6', $hosts->count() );
+        $this->assertSame( '192.168.49.1', (string) $hosts->getFirstIP() );
+        $this->assertSame( '192.168.49.6', (string) $hosts->getlastIP() );
+    }
+
+    public function getIPv6Hosts()
+    {
+        $net = new Network( '2001:db8:85a3:8d3::/64' );
+        $hosts = $net->getHosts();
+
+        $this->assertInstanceOf( Range::class, $hosts );
+        $this->assertSame( '18446744073709551616', $hosts->count() );
+        $this->assertSame( '2001:db8:85a3:8d3::', (string) $hosts->getFirstIP() );
+        $this->assertSame( '2001:db8:85a3:8d3:ffff:ffff:ffff:ffff', (string) $hosts->getlastIP() );
+    }
+}
